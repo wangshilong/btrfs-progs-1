193263f3a42edf70d7c18aef575dc3014b019749
Qu Wenruo <quwenruo@cn.fujitsu.com>
2014-12-03 12:18:31 +0800

    btrfs-progs: Add btrfs_mkdir() function
    With the previous btrfs inode operations patches, now we can use
    btrfs_mkdir() to create the 'lost+found' dir to do some data salvage in
    btrfsck.
    
    This patch along with previous ones will make data salvage easier.
    
    Signed-off-by: Qu Wenruo <quwenruo@cn.fujitsu.com>
    Signed-off-by: David Sterba <dsterba@suse.cz>

--- ctree.h
+++ ctree.h
@@ -2456,5 +2456,7 @@ int btrfs_add_orphan_item(struct btrfs_trans_handle *trans,
 			  u64 ino);
 int btrfs_mkdir(struct btrfs_trans_handle *trans, struct btrfs_root *root,
 		char *name, int namelen, u64 parent_ino, u64 *ino, int mode);
+int btrfs_mkdir(struct btrfs_trans_handle *trans, struct btrfs_root *root,
+		char *name, int namelen, u64 parent_ino, u64 *ino, int mode);
 
 #endif
